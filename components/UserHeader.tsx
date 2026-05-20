a4102d92479bdf95dbb82cf944d439ffe18332e1
yumetsuki <yumetsukigame@gmail.com>
2026-05-20 20:46:42 +0900

    update

--- components/UserHeader.tsx
+++ components/UserHeader.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function UserHeader() {
+  const router = useRouter();
+
   return (
-    <header className="w-full">
+    <header
+      className="w-full cursor-pointer"
+      onClick={() => router.push("/")}
+    >
       <img
         src="/header.webp"
         alt="ゆめつきの書斎 ヘッダー"
-        className="w-full h-auto object-contain"
+        className="w-full h-auto object-contain pointer-events-none"
       />
     </header>
   );
