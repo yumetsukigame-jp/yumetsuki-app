e470b1925bf7153ebbeec035b7db81417db8e299
yumetsuki <yumetsukigame@gmail.com>
2026-06-20 07:51:55 +0900

    update

--- app/admin/rewards/add/page.tsx
+++ app/admin/rewards/add/page.tsx
@@ -1,10 +1,5 @@
 import AddRewardForm from "./AddRewardForm";
 
-export default async function Page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/rewards-images`, {
-    cache: "no-store",
-  });
-  const images = await res.json();
-
-  return <AddRewardForm images={images} />;
+export default function Page() {
+  return <AddRewardForm />;
 }
